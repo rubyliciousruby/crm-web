5b644776113ce3283c87162248a4d227b03eab44
Ruby Li <rubyli@Ruby-Lis-MacBook-Pro-2.local>
2015-08-26 15:03:46 -0400

    updated version

--- crm.rb
+++ crm.rb
@@ -1,24 +1,26 @@
-require_relative 'rolodex'
 require_relative 'contact'
+require_relative 'rolodex'
+
 require 'sinatra'
 
 $rolodex= Rolodex.new
-get '/' do
-  @crm_app_name = "My CRM"
-  erb :index
-end
-
-get '/contacts' do
-
-  erb :contacts
-end
 
 get '/contacts/new' do
 
   erb :new_contact
 end
 
+get '/' do
+  @crm_app_name = "My CRM"
+  erb :index
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   $rolodex.add_contact(new_contact)
+  redirect to ('contacts')
 end
