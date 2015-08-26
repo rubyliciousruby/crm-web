d029cb41974ecbcd326d69133ddc8e67cc38940e
Ruby Li <rubyli@Ruby-Lis-MacBook-Pro-2.local>
2015-08-25 20:57:11 -0400

    crm-web assignment

--- crm.rb
+++ crm.rb
@@ -1,5 +1,24 @@
+require_relative 'rolodex'
+require_relative 'contact'
 require 'sinatra'
 
+$rolodex= Rolodex.new
 get '/' do
+  @crm_app_name = "My CRM"
   erb :index
+end
+
+get '/contacts' do
+
+  erb :contacts
+end
+
+get '/contacts/new' do
+
+  erb :new_contact
+end
+
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  $rolodex.add_contact(new_contact)
 end
